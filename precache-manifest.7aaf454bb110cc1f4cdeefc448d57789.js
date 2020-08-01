dbeede91f6a794e01753352d065f5548fcaa6254
victor.castro.contreras <castro002>
2020-07-31 23:28:28 -0500

    Updates

---
+++ precache-manifest.7aaf454bb110cc1f4cdeefc448d57789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650f61f045a16d18e4fedecb1a008f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e664bf07cb8b90c4abf0",
+    "url": "/static/css/main.aa286cb3.chunk.css"
+  },
+  {
+    "revision": "4ff557f29ca95c66455d",
+    "url": "/static/js/2.dbb0204d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dbb0204d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e664bf07cb8b90c4abf0",
+    "url": "/static/js/main.1d6944ef.chunk.js"
+  },
+  {
+    "revision": "b2a09bde8e96827f0ab6",
+    "url": "/static/js/runtime-main.540fa81a.js"
+  }
+]);
